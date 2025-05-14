9dfc2841f51f168c9524115c8f34c5962b0822d7
hadsu275 <hadisultani275@gmail.com>
2025-05-14 10:59:15 +0200

    gfyctffy

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:imat_app/app_theme.dart';
 import 'package:imat_app/model/imat/product.dart';
 import 'package:imat_app/model/imat_data_handler.dart';
-//import 'package:imat_app/model/imat_data_handler.dart;
 import 'package:imat_app/widgets/center_view.dart';
 import 'package:imat_app/widgets/header.dart';
 import 'package:imat_app/widgets/shopping_cart.dart';
