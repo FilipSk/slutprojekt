074e1e8b2d3b52e6e8e2a3da3d57edf935a401f4
AlexanderSashaa <alexag@chalmers.se>
2025-05-14 10:30:10 +0100

    AppBar

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,21 +1,48 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget implements PreferredSizeWidget{
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return AppBar(
       title: Text("Hanks livs"),
+      backgroundColor: Colors.white,
       actions: [
-
+        Padding(
+          padding: EdgeInsets.only(right: 400.0),
+          //padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: SizedBox(
+            width: 400,
+            height: 40,
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Theme.of(context).colorScheme.primaryContainer,
+                prefixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    print("Du tryckte på sök");
+                    //TODO implementera sök
+                  },
+                ),
+                hintText: "Sök",
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0,
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-  
 }
