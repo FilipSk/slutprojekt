909b6c2f53402e11ef8694a7139cf4b33eaf4d22
EMRMedia <61912708+EMRMedia@users.noreply.github.com>
2025-05-29 10:38:55 +0200

    UserView fix

--- lib/pages/user_view.dart
+++ lib/pages/user_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:imat_app/pages/adress_view.dart';
 import 'package:imat_app/pages/customer_service_view.dart';
 import 'package:imat_app/pages/history_view.dart';
-import 'package:imat_app/pages/main_view.dart';
 import 'package:imat_app/pages/payment_view.dart';
 import 'package:imat_app/widgets/app_bar.dart';
 import 'package:imat_app/widgets/user_button.dart';
@@ -14,84 +13,124 @@ class UserView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(onSearchChanged: (String) {}),
-
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 75),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                UserButton(
-                  title: 'Tillbaka',
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const MainView()),
-                    );
-                  },
-                  color: Colors.red,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 150.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () => Navigator.pop(context),
+                icon: const Icon(Icons.arrow_back),
+                label: const Text('Tillbaka'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 15,
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
                 ),
-                const SizedBox(width: 75),
-                UserButton(
-                  title: 'Kundservice',
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const CustomerServiceView(),
-                      ),
-                    );
-                  },
-                  color: const Color.fromARGB(255, 76, 175, 80),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        UserButton(
+                          icon: const Icon(
+                            Icons.phone,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          title: 'Kundservice',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder:
+                                    (context) => const CustomerServiceView(),
+                              ),
+                            );
+                          },
+                          color: const Color.fromARGB(255, 76, 175, 80),
+                        ),
+                        const SizedBox(width: 80),
+                        UserButton(
+                          icon: const Icon(
+                            Icons.history,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          title: 'Historik',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HistoryView(),
+                              ),
+                            );
+                          },
+                          color: const Color.fromARGB(255, 76, 175, 80),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 70),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        UserButton(
+                          icon: const Icon(
+                            Icons.delivery_dining_rounded,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          title: 'Leveransuppgifter',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const AdressView(),
+                              ),
+                            );
+                          },
+                          color: const Color.fromARGB(255, 76, 175, 80),
+                        ),
+                        const SizedBox(width: 80),
+                        UserButton(
+                          icon: const Icon(
+                            Icons.payment,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          title: 'Betalningsuppgifter',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const PaymentView(),
+                              ),
+                            );
+                          },
+                          color: const Color.fromARGB(255, 76, 175, 80),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 75),
-                UserButton(
-                  title: 'Historik',
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const HistoryView(),
-                      ),
-                    );
-                  },
-                  color: const Color.fromARGB(255, 76, 175, 80),
-                ),
-              ],
-            ),
-            SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                UserButton(
-                  title: 'Leveransuppgifter',
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const AdressView(),
-                      ),
-                    );
-                  },
-                  color: const Color.fromARGB(255, 76, 175, 80),
-                ),
-                const SizedBox(width: 75),
-                UserButton(
-                  title: 'Betalningsuppgifter',
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const PaymentView(),
-                      ),
-                    );
-                  },
-                  color: const Color.fromARGB(255, 76, 175, 80),
-                ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/user_button.dart
+++ lib/widgets/user_button.dart
@@ -4,12 +4,14 @@ class UserButton extends StatelessWidget {
   final String title;
   final VoidCallback onPressed;
   final Color color;
+  final Icon icon;
 
   const UserButton({
     super.key,
     required this.title,
     required this.onPressed,
     required this.color,
+    required this.icon,
   });
 
   @override
@@ -18,18 +20,25 @@ class UserButton extends StatelessWidget {
       children: [
         const SizedBox(height: 32),
         SizedBox(
-          width: 375,
+          width: 425,
           height: 200,
           child: TextButton(
             onPressed: onPressed,
             style: TextButton.styleFrom(backgroundColor: color),
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 255, 255, 255),
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icon,
+                const SizedBox(width: 8),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
